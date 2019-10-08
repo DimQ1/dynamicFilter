9713f7cb5ae553cf2f263db2405c952abd1546b2
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-10-08 13:06:54 +0300

    added drag function to the component

--- src/Filter/Header.js
+++ src/Filter/Header.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons'
 
-export default function Header() {
-    const colorIcons = "#a8a9af";
-    return (
-        <div className="Filter-container__header">
-            <div className="Row-container">
-                <FontAwesomeIcon icon={faBars} rotation={90} color={colorIcons}/>
-                <div className="ml-15 text-bold">FILTERS</div>
+export default class Header extends React.Component {
+    handleMouse = (event) => {
+        this.props.onMousedown(event);
+    }
+    render() {
+        const colorIcons = "#a8a9af";
+        return (
+            <div className="Filter-container__header" onMouseDown={this.handleMouse}>
+                <div className="Row-container">
+                    <FontAwesomeIcon icon={faBars} rotation={90} color={colorIcons} />
+                    <div className="ml-15 text-bold">FILTERS</div>
+                </div>
+                <button>
+                    <FontAwesomeIcon icon={faTimes} color={colorIcons} />
+                </button>
             </div>
-            <button>
-                <FontAwesomeIcon icon={faTimes} color={colorIcons} />
-            </button>
-        </div>
-    );
+        );
+    }
+
 };
--- src/Filter/MainContainer.js
+++ src/Filter/MainContainer.js
@@ -4,35 +4,81 @@ import Header from './Header';
 import Dropdown from './Dropdown';
 import Search from './Search';
 
-export default function MainContainer() {
-    return (
-        <div className="Filter-contaner" draggable>
-            <div className="Header-line_color-blue"></div>
-            <Header />
-            <div className="Row-container Row-container_background-b">
-                <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
-                <div className="Row-container__Col-2">
-                    <Dropdown name={'CONTEXT'} value={["serch1", "search2"]} />
-                </div>
-            </div>
-            <div className="Row-container Row-container_background-b">
-                <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
-                <div className="Row-container__Col-2">
-                    <Dropdown name={'DIMENSIONS'} value={["serch1", "search2"]} />
-                </div>
-            </div>
-            <div className="Row-container Row-container_background-b">
-                <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
-                <div className="Row-container__Col-2">
-                    <Search value={''} />
-                </div>
-            </div>
-            <div className="Row-container">
-                <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
-                <div className="Row-container__Col-2 "></div>
-            </div>
-        </div>
-    );
+export default class MainContainer extends React.Component {
+    constructor(prpos) {
+        super(prpos);
+        this.state = { y: 0, x: 0 };
+    }
 
+    mouseDown = (e) => {
+        if (!e) {
+            return;
+        }
+
+        this.hitOffset = {
+            x: e.pageX - this.state.x,
+            y: e.pageY - this.state.y,
+        };
+        window.addEventListener('mousemove', this.mouseMove);
+        window.addEventListener('mouseup', this.mouseUp);
+        e.preventDefault();
+    }
+
+    mouseMove = (e) => {
+        if (!e) {
+            return;
+        }
+        this.setState((prev) => {
+            const position = {
+                x: e.pageX - this.hitOffset.x,
+                y: e.pageY - this.hitOffset.y
+            };
+            return { x: position.x, y: position.y };
+        })
+    }
+
+    mouseUp = (e) => {
+        if (!e) {
+            return;
+        }
+        window.removeEventListener('mousemove', this.mouseMove);
+        window.removeEventListener('mouseup', this.mouseUp);
+    }
+    render() {
+        const { x, y } = this.state;
+
+        return (
+            <div className="Filter-contaner" style={{ left: x, top: y }}>
+                <div className="Header-line_color-blue"></div>
+                <Header onMousedown={this.mouseDown} />
+                <div className="Row-container Row-container_background-b">
+                    <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
+                    <div className="Row-container__Col-2">
+                        <Dropdown name={'CONTEXT'} value={["serch1", "search2"]} />
+                    </div>
+                </div>
+                <div className="Row-container Row-container_background-b">
+                    <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
+                    <div className="Row-container__Col-2">
+                        <Dropdown name={'DIMENSIONS'} value={["serch1", "search2"]} />
+                    </div>
+                </div>
+                <div className="Row-container Row-container_background-b">
+                    <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
+                    <div className="Row-container__Col-2">
+                        <Search value={''} />
+                    </div>
+                </div>
+                <div className="Row-container">
+                    <div className="Row-container__Col-1 Row-container__Col-1_border-r"></div>
+                    <div className="Row-container__Col-2 "></div>
+                </div>
+            </div >
+        );
+    }
 }
 
+MainContainer.defaultProps = {
+    x: 0,
+    y: 0
+}
